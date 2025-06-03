1463d726de29246d75654f8a1c737ed44da38dab
freez3en <kondemist@gmail.com>
2025-06-03 19:18:03 +0300

    Заглушки что бы не было конфликтов

---
+++ game_logic.py
@@ -0,0 +1,12 @@
+def game(*args, **kwargs):
+    print("Игра пока не готова")
+    return True
+
+def level_up_screen(*args, **kwargs):
+    pass
+
+def trophy_screen(*args, **kwargs):
+    pass
+
+def game_over_screen(*args, **kwargs):
+    return "menu"
