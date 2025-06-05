77605c544ad858bd8c6e4c03100eae3f23b0f62b
freez3en <kondemist@gmail.com>
2025-06-05 21:14:52 +0300

    добавил вызов игры после выбора скина

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ WIDTH, HEIGHT = 480, 640
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("GAME")
 
+img_car = load_image("Car.png", 80, 90).convert_alpha()
+img_obstacle = load_image("obstacle.png", 80, 90).convert_alpha()
+sound_collision = load_sound("collision.wav")
+sound_nitro = load_sound("nitro.wav")
+sound_level_up = load_sound("level_up.wav")
 img_truck = load_image("truck.png", 80, 90).convert_alpha()
 img_car_game = load_image("Car_Game.png", 80, 90).convert_alpha()
 
@@ -23,8 +28,7 @@ def main():
     while True:
         start_level = menu(screen, clock, font_big, font_medium)
         player_image = skin_selection(screen, clock, font_big, img_truck, img_car_game)
-        print(f"Выбран уровень: {start_level}")
-        print(f"Выбран скин: {player_image}")
+        game(start_level, player_image, img_car, img_obstacle, sound_collision, sound_nitro, sound_level_up)
         break
 
 if __name__ == "__main__":
